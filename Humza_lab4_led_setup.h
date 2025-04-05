6a5a9e208c53d275a008f9ffd3c268abed434351
Humza Rana <142959387+HumzaProfessional@users.noreply.github.com>
2025-04-05 02:59:28 -0400

    Update and rename led_setup.h to Humza_lab4_led_setup.h

--- Humza_lab4_led_setup.h
+++ Humza_lab4_led_setup.h
@@ -11,13 +11,16 @@
 
 #include <stdint.h>
 
+// Modes for LED output
+#define SINGLE_LED_MODE 0
+#define FLASH_LED_MODE  1
 // Configure PC0..PC1 as input pull-ups for buttons.
 void init_Buttons(void);
 
 // Configure PC8..PC15 as outputs for LEDs.
-void init_LEDs_PC8to15(void);
+void init_LEDs_PC6to13(void);
 
 // Write an 8-bit pattern to PC8..PC15 (bit 0 => PC8, bit 7 => PC15).
-void update_LEDs_PC8to15(uint8_t pattern);
+void update_LEDs_PC6to13(uint8_t ledPattern, uint8_t led_mode);
 
 #endif // LED_SETUP_H
