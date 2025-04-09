52566f0b542fabc27eb3826ab9dbf8fc7f96bb0f
Humza Rana <142959387+HumzaProfessional@users.noreply.github.com>
2025-04-09 11:00:10 -0400

    Update Final_project_main.c

--- Final_project_main.c
+++ Final_project_main.c
@@ -1,1 +1,15 @@
-
+/* ==============================================================================
+ * @file: main.c
+ * @brief: main program
+ * @ author: Humza Rana & Mac
+ * @Lab 4: 1D pong using leds
+ * @Class: CPE 3000
+ * -----------------------------------------------------
+ *  In this lab, pins are enabled and buttons are used to simulate playing pong.
+ *  Two buttons are enabled to trigger a Systick interrupt.
+ *  This interrupt determines if the player has "swung" or served(if in the serving state)
+ *  State machines are used to determine the following: what state the game is in such as serving.
+ *
+ * 
+ * =================================================================================
+ */
