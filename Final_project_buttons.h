a5120640b0ea618006711a2692a78086eacac382
Humza Rana <142959387+HumzaProfessional@users.noreply.github.com>
2025-04-24 05:25:21 -0400

    Update Final_project_buttons.h

--- Final_project_buttons.h
+++ Final_project_buttons.h
@@ -3,11 +3,13 @@
 
 #include "stm32l476xx.h"
 
+// Definitions that organize the button data positions
 #define NUM_BUTTONS 3
 #define BTN_RIGHT   0
 #define BTN_LEFT    1
 #define BTN_USER    2
 
+// structure which stores all the buttons
 typedef struct {
     uint32_t filter;
     uint32_t state;
@@ -15,8 +17,10 @@ typedef struct {
     uint32_t pin;
 } Button;
 
+// Allows global access to the array
 extern volatile Button buttons[NUM_BUTTONS];
 
+// function for initializing buttons
 void init_Buttons(void);
 
 #endif
